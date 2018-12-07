75ffbca5097d8d06bc9fdfcb448c652c4f767d00
DasRegal <plexus.9310@gmail.com>
2018-12-08 00:26:18 +0700

    Add Open to toolbar

--- src/editor.cpp
+++ src/editor.cpp
@@ -13,10 +13,12 @@ EditorWindow::EditorWindow()
 void EditorWindow::createActions()
 {
     QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
+    QToolBar *fileToolBar = addToolBar(tr("File"));
 
     const QIcon openIcon = QIcon::fromTheme("document-open", QIcon(":/images/open.png"));
     QAction *openAct = new QAction(openIcon, tr("&Open..."), this);
     openAct->setShortcuts(QKeySequence::Open);
     openAct->setStatusTip(tr("Open an existing file"));
     fileMenu->addAction(openAct);
+    fileToolBar->addAction(openAct);
 }
